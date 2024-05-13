c3cb6eb32a581897f97b60e9f20ce7aef0352e33
seongmin <0411tjdals34@gmail.com>
2024-05-13 14:27:58 +0900

    update navigationbar

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,4 +1,3 @@
-// main_screen.dart
 import 'package:flutter/material.dart';
 
 class MainScreens extends StatefulWidget {
@@ -7,34 +6,58 @@ class MainScreens extends StatefulWidget {
 }
 
 class _MainScreensState extends State<MainScreens> {
+  int _selectedIndex = 0; // 현재 선택된 탭의 인덱스를 추적합니다.
+
+  // 각 탭에 해당하는 위젯을 리스트로 관리합니다.
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text('Home', style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+    Text('Graduation',
+        style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+    Text('Bulletin Board',
+        style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+    Text('My Page',
+        style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index; // 사용자가 탭을 선택하면 인덱스를 업데이트합니다.
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Main Screens'),
-        centerTitle: true,
+        title: Text('Academic Management'),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Welcome to the Main Screen!',
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                print('You have clicked the button!');
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.blue,
-                foregroundColor: Colors.white,
-              ),
-              child: Text('Click Me!'),
-            )
-          ],
-        ),
+        // 선택된 탭에 해당하는 위젯을 표시합니다.
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed, // 네 개 이상의 탭을 고정해서 표시
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.school),
+            label: 'Graduation',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.description),
+            label: 'Board',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'My Page',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.blue,
+        onTap: _onItemTapped,
       ),
     );
   }
