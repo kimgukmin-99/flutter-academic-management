b5d97e0f0037ebe4e361e0ea54b8da3e8f244bb5
seongmin <0411tjdals34@gmail.com>
2024-05-24 18:49:29 +0900

    update post_detail

--- lib/screens/bulletin.dart
+++ lib/screens/bulletin.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart';
+import 'package:intl/intl.dart';
 import 'package:academic_management/screens/post_detail_screen.dart';
 import 'dart:io';
 import 'create_post.dart';
@@ -9,12 +9,14 @@ class Post {
   final String content;
   final String? imagePath;
   final String author;
+  final DateTime createdAt;
 
   Post({
     required this.title,
     required this.content,
     this.imagePath,
     required this.author,
+    required this.createdAt,
   });
 }
 
@@ -30,17 +32,20 @@ class _BulletinBoardScreenState extends State<BulletinBoardScreen> {
       content: '이것은 첫 번째 게시물의 내용입니다.',
       imagePath: 'assets/event1.png',
       author: '홍길동',
+      createdAt: DateTime.now().subtract(Duration(days: 1)),
     ),
     Post(
       title: '두 번째 게시물',
       content: '두 번째 게시물의 내용은 더 많은 정보를 담고 있습니다.',
       imagePath: 'assets/event2.png',
       author: '이순신',
+      createdAt: DateTime.now().subtract(Duration(days: 2)),
     ),
     Post(
       title: '세 번째 게시물',
       content: '세 번째 게시물의 내용입니다. 사진은 없습니다.',
       author: '김유신',
+      createdAt: DateTime.now().subtract(Duration(days: 3)),
     ),
   ];
 
@@ -48,106 +53,108 @@ class _BulletinBoardScreenState extends State<BulletinBoardScreen> {
     String? imagePath = image?.path;
     setState(() {
       _posts.insert(
-          0,
-          Post(
-              title: title,
-              content: content,
-              imagePath: imagePath,
-              author: author));
+        0,
+        Post(
+          title: title,
+          content: content,
+          imagePath: imagePath,
+          author: author,
+          createdAt: DateTime.now(),
+        ),
+      );
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Container(
-            padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  '컴퓨터공학과 게시판',
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                ),
-                IconButton(
-                  icon: Icon(Icons.add, color: Colors.black),
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) =>
-                            CreatePostScreen(addPostCallback: _addPost),
-                      ),
-                    );
-                  },
-                ),
-              ],
-            ),
+      appBar: AppBar(
+        title: Align(
+          alignment: Alignment.centerLeft,
+          child: Text(
+            '컴퓨터공학과 게시판',
+            style: TextStyle(color: Colors.black),
           ),
-          Expanded(
-            child: _posts.isEmpty
-                ? const Center(
-                    child: Text(
-                      'No posts yet',
-                      style:
-                          TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+        ),
+        backgroundColor: Color(0xffF5F5F5),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.add, color: Colors.black),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) =>
+                      CreatePostScreen(addPostCallback: _addPost),
+                ),
+              );
+            },
+          ),
+        ],
+      ),
+      body: _posts.isEmpty
+          ? const Center(
+              child: Text(
+                'No posts yet',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+            )
+          : ListView.builder(
+              itemCount: _posts.length,
+              itemBuilder: (context, index) {
+                final post = _posts[index];
+                return Card(
+                  margin: EdgeInsets.all(10),
+                  child: ListTile(
+                    leading: post.imagePath != null
+                        ? Image.asset(
+                            post.imagePath!,
+                            width: 50,
+                            height: 50,
+                            fit: BoxFit.cover,
+                          )
+                        : null,
+                    title: Text(
+                      post.title,
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
                     ),
-                  )
-                : ListView.builder(
-                    itemCount: _posts.length,
-                    itemBuilder: (context, index) {
-                      final post = _posts[index];
-                      return Card(
-                        margin: EdgeInsets.all(10),
-                        child: ListTile(
-                          leading: post.imagePath != null
-                              ? Image.asset(
-                                  post.imagePath!,
-                                  width: 50,
-                                  height: 50,
-                                  fit: BoxFit.cover,
-                                )
-                              : null,
-                          title: Text(
-                            post.title,
-                            maxLines: 1,
-                            overflow: TextOverflow.ellipsis,
+                    subtitle: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          post.content,
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        Text(
+                          '${post.author}',
+                          style: TextStyle(
+                            fontSize: 12,
+                            color: Colors.grey,
                           ),
-                          subtitle: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                post.content,
-                                maxLines: 1,
-                                overflow: TextOverflow.ellipsis,
-                              ),
-                              Text(
-                                '${post.author}',
-                                style: TextStyle(
-                                  fontSize: 12,
-                                  color: Colors.grey,
-                                ),
-                              ),
-                            ],
+                        ),
+                        Text(
+                          DateFormat('yyyy-MM-dd HH:mm').format(post.createdAt),
+                          style: TextStyle(
+                            fontSize: 12,
+                            color: Colors.grey,
                           ),
-                          onTap: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) =>
-                                    PostDetailScreen(post: post),
-                              ),
-                            );
-                          },
+                        ),
+                      ],
+                    ),
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => PostDetailScreen(post: post),
                         ),
                       );
                     },
                   ),
-          ),
-        ],
-      ),
+                );
+              },
+            ),
     );
   }
 }
--- lib/screens/post_detail_screen.dart
+++ lib/screens/post_detail_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:academic_management/screens/bulletin.dart';
+import 'package:intl/intl.dart';
 
 class PostDetailScreen extends StatelessWidget {
   final Post post;
@@ -8,10 +9,12 @@ class PostDetailScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final DateFormat formatter = DateFormat('yyyy-MM-dd HH:mm');
+
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          post.title,
+          '컴퓨터공학과 게시판',
           style: TextStyle(
             fontSize: 20,
             fontWeight: FontWeight.bold,
@@ -32,21 +35,36 @@ class PostDetailScreen extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
+            Text(
+              '${post.author}',
+              style: TextStyle(fontSize: 14.0, color: Colors.grey),
+            ),
+            Text(
+              '${formatter.format(post.createdAt)}',
+              style: TextStyle(fontSize: 14.0, color: Colors.grey),
+            ),
+            SizedBox(height: 10),
+            Text(
+              post.title,
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+              overflow: TextOverflow.ellipsis,
+            ),
             if (post.imagePath != null)
-              Image.asset(
-                post.imagePath!,
-                width: double.infinity,
-                fit: BoxFit.cover,
+              Padding(
+                padding: const EdgeInsets.only(top: 10.0),
+                child: Image.asset(
+                  post.imagePath!,
+                  width: double.infinity,
+                  fit: BoxFit.cover,
+                ),
               ),
             SizedBox(height: 10),
             Text(
               post.content,
               style: TextStyle(fontSize: 16.0),
-            ),
-            SizedBox(height: 5),
-            Text(
-              '${post.author}',
-              style: TextStyle(fontSize: 14.0, color: Colors.grey),
             ),
           ],
         ),
